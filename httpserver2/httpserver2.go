2df767bac33aa23a2e475cf8edc0e30a811e8f6c
jc centos <james@jamescampbell.us>
2018-12-01 15:47:53 -0500

    updated documentation

--- httpserver2/httpserver2.go
+++ httpserver2/httpserver2.go
@@ -5,12 +5,12 @@ import (
 	"net/http"
 )
 
-// Hello : hi!
+// Hello : hi from the webserver at 127.0.0.1:1337
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
 }
 
-// HTTPExample : yep.
+// HTTPExample : Run the example, import into a main using package name
 func HTTPExample() {
 	http.HandleFunc("/", hello)
 	http.ListenAndServe(":1337", nil) // noob server port l0l
