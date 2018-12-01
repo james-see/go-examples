50fbe7a8e93f2fd3bdea500b83c00ddc4de1ea30
jc centos <james@jamescampbell.us>
2018-12-01 15:46:35 -0500

    added print out for each example

--- httpserver2/httpserver2.go
+++ httpserver2/httpserver2.go
@@ -1,4 +1,4 @@
-package http2
+package httpserver2
 
 import (
 	"io"
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
-	http2 "github.com/jamesacampbell/go-examples/http2"
+	"fmt"
+
+	http2 "github.com/jamesacampbell/go-examples/httpserver2"
 	redis1 "github.com/jamesacampbell/go-examples/redis1"
 )
 
 func main() {
+	fmt.Println("Example 1 connects to Redis at local port 6379 and ping / pong.")
 	redis1.Redis1()
+	fmt.Println("Example 2 should be running at http://127.0.0.1:1337")
+	fmt.Println("CTRL + C to exit")
 	http2.HTTPExample()
+	// More coming soon
 }
