47f99e0e5503afb75e0f5de6b8ac854f4fafd5d4
James Campbell <james@jamescampbell.us>
2019-02-09 11:25:41 -0500

    updates to get headliner example working

--- headliner/headliner.go
+++ headliner/headliner.go
@@ -1,15 +1,17 @@
-package main
+package headliner
 
 import (
-    "gopkg.in/jdkato/prose.v2"
+	"fmt"
+
+	prose "gopkg.in/jdkato/prose.v2"
 )
 
-func main() {
-    doc, _ := prose.NewDocument("Lebron James plays basketball in Los Angeles.")
-    for _, ent := range doc.Entities() {
-        fmt.Println(ent.Text, ent.Label)
-        // Lebron James PERSON
-        // Los Angeles GPE
-    }
+// GetEntities get those things from prose.
+func GetEntities() {
+	doc, _ := prose.NewDocument("Lebron James plays basketball in Los Angeles.")
+	for _, ent := range doc.Entities() {
+		fmt.Println(ent.Text, ent.Label)
+		// Lebron James PERSON
+		// Los Angeles GPE
+	}
 }
-
--- main.go
+++ main.go
@@ -3,31 +3,29 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 
-	headless3 "github.com/jamesacampbell/go-examples/headless3"
-	http2 "github.com/jamesacampbell/go-examples/httpserver2"
+	headliner "github.com/jamesacampbell/go-examples/headliner"
 	redis1 "github.com/jamesacampbell/go-examples/redis1"
 )
 
 func main() {
 	fmt.Println("Example 1 connects to Redis at local port 6379 and ping / pong.")
 	redis1.Redis1()
-	var username string
-	if len(os.Args) == 2 {
-		username = os.Args[1]
-	} else {
-		username = "defaultuser"
-	}
-	headless3.Headless3(username)
-	fmt.Println("Example 2 should be running at http://127.0.0.1:1337")
-	fmt.Println("CTRL + C to exit")
-	fmt.Println("Screenshot of instagram account saved to screenshot.png for example 3")
+	// var username string
+	// if len(os.Args) == 2 {
+	// 	username = os.Args[1]
+	// } else {
+	// 	username = "defaultuser"
+	// }
+	// headless3.Headless3(username)
+	// fmt.Println("Example 2 should be running at http://127.0.0.1:1337")
+	// fmt.Println("CTRL + C to exit")
+	// fmt.Println("Screenshot of instagram account saved to screenshot.png for example 3")
 
-	go http2.HTTPExample()
-	time.Sleep(10 * time.Second)
+	// go http2.HTTPExample()
+	// time.Sleep(10 * time.Second)
 
-	headliner()
+	headliner.GetEntities()
 	// More coming soon
 	os.Exit(0)
 }
